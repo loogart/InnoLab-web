36110ab3fe103e614b23756590be4f5db8c00c35
Chris Soueidan <chris@loogart.com>
2016-04-25 10:54:05 -0400

    created common scripts.js

---
+++ scripts.js
@@ -0,0 +1,58 @@
+//fade in effect
+$(document).ready(function () {
+    $('body').css('display', 'none');
+    $('body').fadeIn(1500);
+    $('.pure-menu-link').click(function (event) {
+        event.preventDefault();
+        newLocation = this.href;
+        $('body').fadeOut(500, newpage);
+    });
+
+    function newpage() {
+        window.location = newLocation;
+    }
+});
+//drawer toggle
+$(document).ready(function () {
+    $(".toggle-drawer").click(function () {
+        $('.toggle').toggleClass('drawer-closed drawer-open');
+        $('.backdrop').toggleClass('like-its-hot');
+        $('body').toggleClass('scrollable not-scrollable');
+    });
+});
+//drawer ESC toggle
+$(document).on('keydown', function (e) {
+    if (e.keyCode === 27) {
+        $(".drawer-open").toggleClass('drawer-closed drawer-open');
+        $('.backdrop').removeClass('like-its-hot');
+        $('body').removeClass('not-scrollable');
+    }
+});
+//parallax effect
+$(window).stellar({
+    horizontalScrolling: false,
+    responsive: false
+});
+//scroll change navbar color
+$(document).ready(function () {
+    //var limit = $('.navbar').height() + $('.j1').height();
+    var limit1 = $(window).height();
+    $(window).scroll(function () {
+        var scrollVal = $(this).scrollTop();
+        if (scrollVal > limit1) {
+            $('.navbar').addClass('navbar-inverse');
+        } else {
+            $('.navbar').removeClass('navbar-inverse');
+        }
+    });
+    $(window).resize(function () {
+        limit1 = $(window).height();
+    });
+});
+// menu horizontal
+(function (window, document) {
+    document.getElementById('toggle').addEventListener('click', function (e) {
+        document.getElementById('tuckedMenu').classList.toggle('custom-menu-tucked');
+        document.getElementById('toggle').classList.toggle('x');
+    });
+})(this, this.document);
