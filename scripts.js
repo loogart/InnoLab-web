e5932ba6dba8c3616843d7c63b37ec30f6e01cbc
Chris Soueidan <chris@loogart.com>
2016-05-12 14:40:34 -0400

    final fixes for layout and added google forms

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
 $(document).ready(function () {
     $('body').css('display', 'none');
     $('body').fadeIn(50);
-    $('.pure-menu-link').click(function (event) {
+    $('.link').click(function (event) {
         event.preventDefault();
         newLocation = this.href;
         $('body').fadeOut(250, newpage);
